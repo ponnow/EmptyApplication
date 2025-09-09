6211bf4e38bb96501d098ca3aa37fa89a9b075f1
Nazmul Hasan <Nazmul Hasan@NAZMUL-IT>
2025-09-09 17:06:44 +0600

    Created Movies Contorler, Movie model & Save view. Also created a form and validation.

---
+++ ContactUsApplication/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using ContactUsApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactUsApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Save()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(Movie movie)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ ContactUsApplication/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactUsApplication.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+
+    }
+}
